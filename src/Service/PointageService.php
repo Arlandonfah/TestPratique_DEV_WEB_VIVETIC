58876ec8ce83c22fe63b530c3fe07ca0e690412b
Arlandon fah <arlandon.dev@gmail.com>
2025-07-03 13:23:59 +0300

    refactor: ajout de quelque logs sur le service

--- src/Service/PointageService.php
+++ src/Service/PointageService.php
@@ -61,99 +61,91 @@ class PointageService
         return $result;
     }
 
-    public function processDailyLogs(array $logs, \DateTimeInterface $date): array
-    {
-        $collaborateurs = [];
-        $nextDay = (clone $date)->modify('+1 day');
-        $previousDay = (clone $date)->modify('-1 day');
+    public function processDailyLogs(array $logs, \DateTimeInterface $selectedDate): array
+{
+    $collaborateurs = [];
 
-        foreach ($logs as $log) {
-            $pin = $log->getPin();
-            $logTime = $log->getTime();
+    foreach ($logs as $log) {
+        $pin = $log->getPin();
+        $time = $log->getTime();
 
-            if (!isset($collaborateurs[$pin])) {
-                $collaborateurs[$pin] = [
-                    'name' => $log->getName(),
-                    'pin' => $pin,
-                    'cards' => [],
-                    'entries' => [],
-                    'exits' => [],
-                    'overnight' => false
-                ];
-            }
-
-            $collaborateurs[$pin]['cards'][$log->getCardNo()] = true;
-
-            // Gestion des entrées/sorties sur plusieurs jours
-            if ($log->getEventPointName() === 'entrée') {
-                // Si l'entrée est avant minuit mais la sortie serait après
-                if ($logTime < $date && !$collaborateurs[$pin]['entries']) {
-                    $collaborateurs[$pin]['overnight'] = true;
-                }
-                $collaborateurs[$pin]['entries'][] = $logTime;
-            } elseif ($log->getEventPointName() === 'sortie') {
-                $collaborateurs[$pin]['exits'][] = $logTime;
-            }
-        }
-
-        $result = [];
-        foreach ($collaborateurs as $pin => $data) {
-            $entries = $data['entries'];
-            $exits = $data['exits'];
-
-            // Trier par ordre chronologique
-            usort($entries, function($a, $b) { return $a <=> $b; });
-            usort($exits, function($a, $b) { return $a <=> $b; });
-
-            // Première entrée
-            $firstEntry = $entries ? min($entries) : null;
-
-            // Dernière sortie
-            $lastExit = $exits ? max($exits) : null;
-
-            // Calcul des pauses
-            $pauses = $this->calculatePauses($entries, $exits);
-
-            // Formatage de la durée des pauses
-            $pauseDuration = $this->formatDuration($pauses['duration']);
-
-            $result[] = [
-                'name' => $data['name'],
+        if (!isset($collaborateurs[$pin])) {
+            $collaborateurs[$pin] = [
+                'name' => $log->getName(),
                 'pin' => $pin,
-                'cards' => implode(',', array_keys($data['cards'])),
-                'firstEntry' => $firstEntry,
-                'lastExit' => $lastExit,
-                'pauseCount' => $pauses['count'],
-                'pauseDuration' => $pauseDuration,
-                'overnight' => $data['overnight']
+                'cards' => [],
+                'entries' => [],
+                'exits' => []
             ];
         }
 
-        return $result;
-    }
-
-    private function calculatePauses(array $entries, array $exits): array
-    {
-        $pauses = ['count' => 0, 'duration' => 0];
-        $exitIndex = 0;
-
-        for ($i = 1; $i < count($entries); $i++) {
-            while ($exitIndex < count($exits) && $exits[$exitIndex] < $entries[$i]) {
-                $exitTime = $exits[$exitIndex];
-                $entryTime = $entries[$i];
-
-                if ($exitTime < $entryTime) {
-                    $pauses['count']++;
-                    $pauses['duration'] += ($entryTime->getTimestamp() - $exitTime->getTimestamp());
-                    break;
-                }
-                $exitIndex++;
-            }
+        // Ajout de la carte si elle n'est pas déjà présente
+        if (!in_array($log->getCardNo(), $collaborateurs[$pin]['cards'])) {
+            $collaborateurs[$pin]['cards'][] = $log->getCardNo();
         }
 
-        // Convertir la durée en secondes
-        $pauses['duration'] = $pauses['duration'] ?: 0;
-
-        return $pauses;
+        // Stockage des entrées et sorties
+        if ($log->getEventPointName() === 'entrée') {
+            $collaborateurs[$pin]['entries'][] = $time;
+        } elseif ($log->getEventPointName() === 'sortie') {
+            $collaborateurs[$pin]['exits'][] = $time;
+        }
     }
+
+    $result = [];
+    foreach ($collaborateurs as $pin => $data) {
+        // Triage des horaires
+        usort($data['entries'], function($a, $b) {
+            return $a <=> $b;
+        });
+        usort($data['exits'], function($a, $b) {
+            return $a <=> $b;
+        });
+
+        // Première entrée
+        $firstEntry = $data['entries'] ? min($data['entries']) : null;
+        
+        // Dernière sortie
+        $lastExit = $data['exits'] ? max($data['exits']) : null;
+        
+        // Calcule des pauses
+        $pauses = $this->calculatePauses($data['entries'], $data['exits']);
+
+        $result[] = [
+            'name' => $data['name'],
+            'pin' => $pin,
+            'cards' => $data['cards'],
+            'firstEntry' => $firstEntry,
+            'lastExit' => $lastExit,
+            'pauseCount' => $pauses['count'],
+            'pauseDuration' => $pauses['duration']
+        ];
+    }
+
+    return $result;
+}
+
+private function calculatePauses(array $entries, array $exits): array
+{
+    $pauses = ['count' => 0, 'duration' => 0];
+    $exitIndex = 0;
+
+    // On suppose que les tableaux sont triés par ordre chronologique
+    for ($i = 1; $i < count($entries); $i++) {
+       
+        while ($exitIndex < count($exits) && $exits[$exitIndex] < $entries[$i]) {
+            // Vérification si cette sortie est après l'entrée précédente
+            if ($exits[$exitIndex] > $entries[$i - 1]) {
+                $pauses['count']++;
+                $pauses['duration'] += ($entries[$i]->getTimestamp() - $exits[$exitIndex]->getTimestamp());
+                break;
+            }
+            $exitIndex++;
+        }
+    }
+
+    return $pauses;
+}
+
+  
 }
